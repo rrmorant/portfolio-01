b5301a1a5b168ad33f9ebe7fa3d0940c5504489d
Ron M <duecel@icloud.com>
2024-12-20 22:21:30 -0800

    progressing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Tech from "./components/Tech";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
 function App() {
 	return (
 		<>
-			<div className="text-8xl font-bold">Hello World</div>
+			<main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
+				<Navbar />
+				<Hero />
+				<Tech />
+				<Projects />
+				<Contact />
+			</main>
 		</>
 	);
 }
