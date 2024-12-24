171145c9ee6d92a2184adef818518aa63bea600e
Ron M <duecel@icloud.com>
2024-12-24 13:32:46 -0800

    tech component functional

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,82 @@
+import {
+	BiLogoDocker,
+	BiLogoJavascript,
+	BiLogoNodejs,
+	BiLogoPostgresql,
+	BiLogoTailwindCss,
+	BiLogoTypescript,
+} from "react-icons/bi";
+import { motion } from "motion/react";
+
 const Tech = () => {
-	return <div>Tech</div>;
+	const variants = {
+		hidden: { opacity: 0, y: 50 },
+		visible: { opacity: 1, y: 50 },
+	};
+
+	return (
+		<div
+			id="tech"
+			className="flex min-h-[70vh] w-full flex-col items-center justify-center gap-16 md:gap-32">
+			<motion.h1
+				variants={variants}
+				initial="hidden"
+				whileInView="visible"
+				transition={{ duration: 0.5 }}
+				className="text-4xl font-light text-white md:text-6xl">
+				Technologies
+			</motion.h1>
+			<div className="flex flex-wrap items-center justify-center gap-10 p-5">
+				<motion.div
+					variants={variants}
+					initial="hidden"
+					whileInView="visible"
+					transition={{ duration: 0.5 }}>
+					<BiLogoTypescript className="cursor-pointer text-[80x] text-sky-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
+				</motion.div>
+
+				<motion.div
+					variants={variants}
+					initial="hidden"
+					whileInView="visible"
+					transition={{ duration: 0.5 }}>
+					<BiLogoJavascript className="cursor-pointer text-[80x] text-orange-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
+				</motion.div>
+
+				<motion.div
+					variants={variants}
+					initial="hidden"
+					whileInView="visible"
+					transition={{ duration: 0.5 }}>
+					<BiLogoDocker className="cursor-pointer text-[80x] text-blue-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
+				</motion.div>
+
+				<motion.div
+					variants={variants}
+					initial="hidden"
+					whileInView="visible"
+					transition={{ duration: 0.5 }}>
+					<BiLogoTailwindCss className="cursor-pointer text-[80x] text-sky-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
+				</motion.div>
+
+				<motion.div
+					variants={variants}
+					initial="hidden"
+					whileInView="visible"
+					transition={{ duration: 0.5 }}>
+					<BiLogoNodejs className="cursor-pointer text-[80x] text-green-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
+				</motion.div>
+
+				<motion.div
+					variants={variants}
+					initial="hidden"
+					whileInView="visible"
+					transition={{ duration: 0.5 }}>
+					<BiLogoPostgresql className="cursor-pointer text-[80x] text-blue-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]" />
+				</motion.div>
+			</div>
+		</div>
+	);
 };
 
 export default Tech;
