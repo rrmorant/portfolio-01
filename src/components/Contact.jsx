dc7af8e0c409e2d9a9ac10820c09f89b8ae94766
Ron M <duecel@icloud.com>
2024-12-28 08:44:10 -0800

    contact component functional

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
 					whenever I can.
 				</p>
 				<a
-					href="mailto:duecel@icloud.com"
+					href="mailto:duecel@hotmail.com"
 					className="text-nowrap rounded-lg border border-indigo-600 bg-black px-5 py-3 text-lg font-bold text-white shadow-lg shadow-indigo-700 transition-all duration-300 hover:-translate-y-2 hover:shadow-x1 hover:shadow-indigo-600">
 					Contact Me
 				</a>
